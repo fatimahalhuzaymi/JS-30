1cf4c43f6b311d66fc0abb05f128acf3d0b68ff7
fatimahalhuzaymi <103121587+fatimahalhuzaymi@users.noreply.github.com>
2022-06-01 23:10:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+
+
+"use strict";
+const ps = require("prompt-sync");
+const prompt = ps();
+
+//1
+//const name = prompt("Enter your name: ");
+//console.log(name +" "+"Hello Welcome!");
+ 
+
+//2
+//const num1 = parseInt(prompt( "Please Enter the first number: ")); 
+//const num2 = parseInt(prompt("Please Enter the second number: "));
+//const sum = num1 + num2;
+//console.log(sum);
+
+
+//3
+
+//const number = parseInt(prompt("Please Enter number: ")); 
+//if (number %2 ==0){
+  //  console.log("number is even");
+
+//}else {
+  //  console.log("number is odd");
+//}
+
+//4
+//const number = parseInt(prompt("Enter a positive number: "));
+//let num1 = 0, num2 = 1, nextTerm;
+
+//console.log("Fibonacci Series: ");
+//console.log(num1); 
+//console.log(num2); 
+
+//nextTerm = num1 + num2;
+
+//while (nextTerm <= number) {
+
+    // print the next term
+  //  console.log(nextTerm);
+
+    //num1 = num2;
+    //num2 = nextTerm;
+    //nextTerm = num1 + num2;
+//}
+
+
+
+
+//5
+
+
+
+// take input from the user
+//const number = parseInt(prompt('Enter a positive integer: '));
+
+
+//if (number < 0) {
+  //  console.log('Error! Factorial for negative number does not exist.');
+//}
+
+//else if (number === 0) {
+   // console.log(`The factorial of ${number} is 1.`);
+//}
+
+//else {
+  //  let fact = 1;
+    //for (i = 1; i <= number; i++) {
+      //  fact *= i;
+    //}
+    //console.log(`The factorial of ${number} is ${fact}.`);
+//}
